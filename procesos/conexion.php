a40a5ed6ebf7ffb0fe8beedaa382a9143b9bd429
robertgar <50602861+robertgar@users.noreply.github.com>
2021-09-28 10:04:56 -0600

    Update conexion.php

--- procesos/conexion.php
+++ procesos/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host='localhost';
-$user='root';
-$password='';
-$db='clinica';
+$host='MYSQL5031.site4now.net';
+$user='a54053_clinica';
+$password='Lamchay2021';
+$db='db_a54053_clinica';
 $conection=@mysqli_connect($host,$user,$password,$db);
 //mysqli_close($conection);
 if (!$conection) {
