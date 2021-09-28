3bde6285093d7de9f7f29deb9840551a54244822
robertgar <50602861+robertgar@users.noreply.github.com>
2021-09-28 10:32:46 -0600

    Update menu2.php

--- menu2.php
+++ menu2.php
@@ -5,7 +5,7 @@
 	 <li><a href="procesos/cerrar.php"><span class="glyphicon glyphicon-off"></span>   Cerrar Sesion</a>
 </li>
 
-  <li><a class="active" href="Mepac.php"><span class="fa fa-user-md" style="font-size:20px"></span>  Lista de Pacientes</a></li>
+  <li><a class="active" href="mepac.php"><span class="fa fa-user-md" style="font-size:20px"></span>  Lista de Pacientes</a></li>
   <li><a  href="lab.php"><span class="glyphicon glyphicon-list-alt"></span>  Ver resultado</a></li>
 
 </ul>
